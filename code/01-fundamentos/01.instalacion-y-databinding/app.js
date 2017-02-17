3b67e896d64828be65d3e70a1d254ab3c204f063
BZGemerson <henryingtel@gmail.com>
2017-02-17 11:00:13 -0500

    Iniciando con el curso de Vue JS
    Comparación entre javascript puro y Vue JS, explicando su
    funcionamiento.

---
+++ code/01-fundamentos/01.instalacion-y-databinding/app.js
@@ -0,0 +1,20 @@
+// Usando conceptos de VANILLA JS estableceremos un 
+// binding entre javascript y el html
+
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
+
+// input.addEventListener( 'keyup', function () {
+// 	h1.innerHTML = input.value;
+// });
+
+// Usando el framework VueJS
+// 
+
+new Vue({
+	el: 'main',
+	data: {
+		mensaje: 'Hola mundo!!!'
+	}
+});
+
