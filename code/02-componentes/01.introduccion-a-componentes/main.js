34d0f34b45cfd3afa3bfc3f5cc560bb87ec6e462
BZGemerson <henryingtel@gmail.com>
2017-02-19 00:36:26 -0500

    primer ejemplo de componentes con vue: completo
    Ejemplo terminado, basado en componentes.

--- code/02-componentes/01.introduccion-a-componentes/main.js
+++ code/02-componentes/01.introduccion-a-componentes/main.js
@@ -1,28 +1,24 @@
 Vue.component('mis-tareas', {
-	props: ['tareas'],
 	template: `
 		<ul>
 			<li v-for="tarea in tareas">
 				{{ tarea.title }}
 			</li>
 		</ul>
-	`
-});
-
-new Vue({
-	el: 'main',
-	data: {
-		tareasAjax: [],
-		tareasLocales: [
-			{ title: 'Hacer las compras' },
-			{ title: 'Aprender vuejs' },
-			{ title: 'Aprender javascript ES6' },
-		]
+	`,
+	data() {
+		return {
+			tareas: []
+		}
 	},
 	mounted() {
 		axios.get('https://jsonplaceholder.typicode.com/todos')
 			.then((respuesta)=>{
-				this.tareasAjax = respuesta.data;
+				this.tareas = respuesta.data;
 			});
 	}
+});
+
+new Vue({
+	el: 'main'	
 });
