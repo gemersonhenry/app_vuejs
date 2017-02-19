14c688b3aeeeedca5c7041f04c128836f0089427
BZGemerson <henryingtel@gmail.com>
2017-02-19 11:08:51 -0500

    Agregando validación de propiedades
    Realizamos el primer ejemplo visible donde implementado con bootstrap
    tiene una apariencia y funcionalidad buena.

---
+++ code/02-componentes/04.validacion-de-propiedades/main.js
@@ -0,0 +1,54 @@
+Vue.component('user-card', {
+	props: {
+		user: {
+			type: [String, Array, Object],
+			required: true
+		}
+	},
+	template: `
+		<transition name="usercard" mode="out-in">
+			<div class="col-xs-12 col-sm-6 col-md-4" v-show="userState">
+				<div class="thumbnail">
+					<img :src="userLocal.picture.large" :alt="userLocal.email">
+					<div class="caption">
+						<h4 class="text-center"> 
+							{{ userLocal.name.first }} {{ userLocal.name.last }} 
+						</h4>
+						<p>
+							<ul>
+								<li> Location: {{ userLocal.location.street }} </li>
+								<li> City: {{ userLocal.location.city }} </li>
+								<li> State: {{ userLocal.location.state }} </li>
+								<li> Postcode: {{ userLocal.location.postcode }} </li>
+							</ul>
+						</p>
+						<p class="text-center">
+							<button class="btn btn-primary btn-sm" @click=" userState = !userState ">
+								Ocultar Persona
+							</button>
+						</p>				
+					</div>
+				</div>
+			</div>
+		</transition>			
+	`,
+	data() {
+		return {
+			userLocal: this.user,
+			userState: true,
+		};
+	}
+});
+
+new Vue({
+	el: '#contenido',
+	data: {
+		users: []
+	},
+	mounted() {
+		axios.get('https://randomuser.me/api/?results=100')
+			.then((response) => {
+				this.users = response.data.results;
+			});
+	}
+});
